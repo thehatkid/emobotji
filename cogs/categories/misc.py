e6496322b0c330d842c721d8df0749d0e652cc07
Sasha Ermohin <saha22023@gmail.com>
2022-03-13 03:07:50 +0700

    Update misc.py

--- cogs/categories/misc.py
+++ cogs/categories/misc.py
@@ -3,6 +3,7 @@ import yaml
 import re
 from io import BytesIO
 import aiohttp
+from utils.database import Database
 import disnake
 from disnake.ext import commands
 
@@ -17,7 +18,7 @@ class CogCommandsMisc(commands.Cog):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
-        self.db = bot.db
+        self.db: Database = bot.db
         self.http = aiohttp.ClientSession(
             loop=bot.loop, read_timeout=10,
             headers={'User-Agent': cfg['bot']['user-agent']}
@@ -39,19 +40,24 @@ class CogCommandsMisc(commands.Cog):
         if not re.fullmatch(r'\w{2,32}', name, re.ASCII):
             return await ctx.reply(f':x: `{name}` is not a valid emoji name; use 2–32 English letters, numbers and underscores.', mention_author=False)
 
-        row = await self.db.fetch_one('SELECT `id`, `name`, `animated`, `nsfw`, `author_id`, `created` FROM `emojis` WHERE `name` = :name', {'name': name})
-        if row is None:
+        emoji = await self.db.get_emoji(name)
+        if emoji is None:
             await ctx.reply(f':x: Emoji `{name}` not exists in bot.', mention_author=False)
         else:
             embed = disnake.Embed(title=f':information_source: Emoji `{name}`', colour=disnake.Colour.blurple())
-            if not is_nsfw and row[3]:
+            if not is_nsfw and emoji['nsfw']:
                 embed.description = '*Preview of Emoji are unavailable because that\'s NSFW*'
             else:
-                embed.set_image(url='https://cdn.discordapp.com/emojis/{0}.{1}?size=512'.format(row[0], 'gif' if row[2] else 'png'))
-            embed.add_field(name='Uploaded by:', value='<@{0}>{1}'.format(row[4], ' *(is you!)*' if int(row[4]) == ctx.author.id else ''), inline=False)
-            embed.add_field(name='Created at:', value='<t:{0}:f> (<t:{0}:R>)'.format(int(row[5].timestamp())), inline=False)
-            embed.add_field(name='Animated?', value='Yes' if row[2] else 'No', inline=False)
-            embed.add_field(name='NSFW?', value='Yes' if row[3] else 'No', inline=False)
+                embed.set_image(url='https://cdn.discordapp.com/emojis/{0}.{1}?size=512'.format(
+                    emoji['id'],
+                    'gif' if emoji['animated'] else 'png'
+                ))
+            embed.add_field(name='Uploaded by:', value='<@{0}>{1}'.format(
+                emoji['author_id'], ' *(is you!)*' if emoji['author_id'] == ctx.author.id else ''
+            ), inline=False)
+            embed.add_field(name='Created at:', value='<t:{0}:f> (<t:{0}:R>)'.format(int(emoji['created_at'].timestamp())), inline=False)
+            embed.add_field(name='Animated?', value='Yes' if emoji['animated'] else 'No', inline=False)
+            embed.add_field(name='NSFW?', value='Yes' if emoji['nsfw'] else 'No', inline=False)
             await ctx.reply(embed=embed, mention_author=False)
 
     @commands.command(name='big', description='Posts full-sized image of emoji.')
@@ -62,37 +68,37 @@ class CogCommandsMisc(commands.Cog):
             is_nsfw = True if ctx.channel.is_nsfw() else False
         if not re.fullmatch(r'\w{2,32}', name, re.ASCII):
             return await ctx.reply(f':x: `{name}` is not a valid emoji name; use 2–32 English letters, numbers and underscores.', mention_author=False)
-        row = await self.db.fetch_one('SELECT `id`, `name`, `animated`, `nsfw` FROM `emojis` WHERE `name` = :name', {'name': name})
-        if row is None:
+        emoji = await self.db.get_emoji(name)
+        if emoji is None:
             await ctx.reply(f':x: Emoji `{name}` not exists in bot.', mention_author=False)
         else:
-            if not is_nsfw and row[3]:
+            if not is_nsfw and emoji['nsfw']:
                 await ctx.reply(':x: Emoji are unavailable for posting here because that\'s **NSFW**.', mention_author=False)
             else:
-                image_format = 'gif' if row[2] else 'png'
-                url = 'https://cdn.discordapp.com/emojis/{0}.{1}?size=512'.format(row[0], image_format)
+                image_format = 'gif' if emoji['animated'] else 'png'
+                url = 'https://cdn.discordapp.com/emojis/{0}.{1}?size=512'.format(emoji['id'], image_format)
                 async with self.http.get(url) as response:
                     image = BytesIO(await response.read())
                 await ctx.send(
-                    content='<{0}:{1}:{2}>'.format('a' if row[2] else '', row[1], row[0]),
-                    file=disnake.File(fp=image, filename='{0}.{1}'.format(row[1], image_format))
+                    content='<{0}:{1}:{2}>'.format('a' if emoji['animated'] else '', emoji['name'], emoji['id']),
+                    file=disnake.File(fp=image, filename='{0}.{1}'.format(emoji['name'], image_format))
                 )
 
     @commands.command(name='react', description='Reacts message with emoji.')
-    async def cmd_react(self, ctx: commands.Context, emoji: str, message_id: int):
+    async def cmd_react(self, ctx: commands.Context, name: str, message_id: int):
         try:
             msg = await ctx.fetch_message(message_id)
         except disnake.errors.NotFound:
             await ctx.reply(':x: Message not found or not from this channel.', mention_author=False)
         else:
-            row = await self.db.fetch_one('SELECT `id`, `nsfw` FROM `emojis` WHERE `name` = :name', {'name': emoji})
-            if row is None:
-                await ctx.reply(f':x: Emoji with name `{emoji}` not found in bot.', mention_author=False)
+            emoji = await self.db.get_emoji(name)
+            if emoji is None:
+                await ctx.reply(f':x: Emoji with name `{name}` not found in bot.', mention_author=False)
             else:
-                if row[1]:
+                if emoji['nsfw']:
                     await ctx.reply(':x: You can\'t react message because your selected emoji was marked as **NSFW only**.', mention_author=False)
                 else:
-                    await msg.add_reaction(self.bot.get_emoji(row[0]))
+                    await msg.add_reaction(self.bot.get_emoji(emoji['id']))
 
 
 def setup(bot):
